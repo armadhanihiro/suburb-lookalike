45ecdaa554afe7a9db93ce61c8ce26cacf4f2032
Armadhani Hiro <hiroarmadhani@gmail.com>
2026-07-11 02:37:42 +0930

    Prepare Streamlit Cloud deployment

--- db/bigquery_client.py
+++ db/bigquery_client.py
@@ -1,12 +1,28 @@
 import os
-from google.cloud import bigquery
-from dotenv import load_dotenv
 
-load_dotenv()
+import streamlit as st
+from google.cloud import bigquery
+from google.oauth2 import service_account
+
 
 def get_bigquery_client():
-    credentials_path = os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
-    if not credentials_path:
-        raise ValueError("GOOGLE_APPLICATION_CREDENTIALS is not set in .env")
+    if "gcp_service_account" in st.secrets:
+        credentials = service_account.Credentials.from_service_account_info(
+            dict(st.secrets["gcp_service_account"])
+        )
 
-    return bigquery.Client()
+        return bigquery.Client(
+            credentials=credentials,
+            project=credentials.project_id,
+        )
+
+    credentials_path = os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
+
+    if credentials_path:
+        return bigquery.Client.from_service_account_json(
+            credentials_path
+        )
+
+    raise RuntimeError(
+        "BigQuery credentials are not configured."
+    )
